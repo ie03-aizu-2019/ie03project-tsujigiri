3e409e8616b0bef19a38eebcf7965910e26a396d
s1250103 <s1250103@u-aizu.ac.jp>
2019-06-01 23:54:27 +0900

    クラス図を足しました。

--- src/Point.py
+++ src/Point.py
@@ -5,28 +5,13 @@
         using 2 coordinates, x and y
                                         """
 
-
-import sys
-args = sys.argv
-"""
-if(args[1] != "-p"):
-    ept = True
-"""
 class Point:
     def __init__(self, x, y):
         self.x = x
         self.y = y
-"""
-    @classmethod
-    def get_Points(self, x, y):
-        self.x = x
-        self.y = y
 
-        return x, y
 """
-try:
-    ept = ept
-except: # debug mode
     if __name__ == '__main__':
         point = Point(0, 0)
         print("x =", point.x,",y =", point.y)
+"""
